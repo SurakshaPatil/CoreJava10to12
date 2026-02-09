2d3c2637a5f279780de7fda889aa2b5aaf9b7d84
SurakshaPatil <surakshapatil1998@gmail.com>
2026-02-09 12:21:14 +0530

    Initial commit

---
+++ src/Variables/DifferentDatatypes.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class DifferentDatatypes {
+
+	public static void main(String[] args) {
+	int a=10;
+	double b=10.10;
+	char c ='h';
+	boolean flag = true;
+	String name="Suraksha";
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(flag);
+	System.out.println(name);
+	}
+
+}
