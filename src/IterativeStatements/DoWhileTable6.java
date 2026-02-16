4dd856cd4f0a244a0c2b69a75469c68d0b11290d
SurakshaPatil <surakshapatil1998@gmail.com>
2026-02-16 12:16:52 +0530

    Iterative statement

---
+++ src/IterativeStatements/DoWhileTable6.java
@@ -0,0 +1,14 @@
+package IterativeStatements;
+
+public class DoWhileTable6 {
+
+	public static void main(String[] args) {
+		int i=1;
+		int a=6;
+		do {
+			System.out.println(a+"*"+i+"="+a*i);
+			i++;
+		} while(i<=10);
+	}
+
+}
